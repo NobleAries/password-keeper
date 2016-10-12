7ff7791773737a8588f467391754d51bc0d8ef7b
Nyderx <faber@student.agh.edu.pl>
2016-10-12 20:55:16 +0200

    Issue-6: Design and create authentication API

--- src/main/java/model/authentication/Authenticator.java
+++ src/main/java/model/authentication/Authenticator.java
@@ -1,16 +1,24 @@
 package model.authentication;
 
 import java.io.*;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.logging.Logger;
 
 public class Authenticator {
+    private final static Logger LOGGER = Logger.getLogger(Authenticator.class.getName());
+
     private final File passwordFile;
     private final MessageDigest messageDigest;
 
     /**
-     * Class for serving user authentication. It uses SHA-512 algorithm.
+     * Class for serving user authentication. It uses SHA-512 algorithm for generating password hash.
      * @param passwordFilePath Path to authentication password file
      * @throws NoSuchAlgorithmException SHA-512 algorithm was not found
      */
@@ -20,19 +28,18 @@ public class Authenticator {
     }
 
     /**
-     * Checks if password is already set.
-     * @return true if file with main password exists or false if not
+     * @return true if password is set or false if it has not been set yet
      */
     public boolean isPasswordSet() {
         return passwordFile.exists();
     }
 
     /**
-     * Sets password the first time. It assumes that program's data directory already exists.
+     * Sets password the first time.
      * @param password First password given by user.
      * @throws IOException
      */
-    public void setFirstPassword(String password) throws IOException {
+    public void setFirstPassword(char[] password) throws IOException {
         if (isPasswordSet()) {
             throw new IllegalStateException("Main password already set");
         }
@@ -53,43 +60,47 @@ public class Authenticator {
     }
 
     /**
-     * Changes password.
-     * @param newPassword
+     *
+     * @param newPassword New password
      * @throws IOException
      */
-    public void changePassword(String newPassword) throws IOException {
+    public void changePassword(char[] newPassword) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(passwordFile));
         bufferedWriter.write(hashPassword(newPassword));
         bufferedWriter.close();
     }
 
     /**
-     * Checks if given password is the same as saved.
-     * @param givenPassword Password which was used by user to isPasswordValid.
-     * @return true if password are same, else other way
+     *
+     * @param givenPassword Password to cheeck if it's valid
+     * @return true if password is valid, else other way
      * @throws IOException
      */
-    public boolean isPasswordValid(String givenPassword) throws IOException {
+    public boolean isPasswordValid(char[] givenPassword) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(passwordFile));
         String password = bufferedReader.readLine();
         bufferedReader.close();
         return password.equals(hashPassword(givenPassword));
     }
 
-    private String hashPassword(String password) {
-        byte[] mdBytes = messageDigest.digest(password.getBytes());
+    private String hashPassword(char[] password) {
+        ByteBuffer byteBuffer = Charset.forName("UTF-8").encode(CharBuffer.wrap(password));
+        byte[] passwordBytes = new byte[byteBuffer.remaining()];
+        byteBuffer.get(passwordBytes);
+        Arrays.fill(byteBuffer.array(), (byte) 0);
 
-        StringBuffer stringBuffer = new StringBuffer();
-        for (int i = 0; i < mdBytes.length; i++) {
-            stringBuffer.append(Integer.toString((mdBytes[i] & 0xff) + 0x100, 16).substring(1));
+        byte[] mdBytes = messageDigest.digest(passwordBytes);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (byte b: mdBytes) {
+            stringBuilder.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
         }
-        return stringBuffer.toString();
+        return stringBuilder.toString();
     }
 
     private void processOutputValueOfSettingPermissions(boolean outputValue) throws IOException {
         if (!outputValue) {
-            passwordFile.delete();
-            throw new IOException("Could not set permissions for main password file");
+            LOGGER.warning("Could not set permission");
         }
     }
 }
